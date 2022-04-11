f62c6bd88c8b9748cc9049b85eb850a276588f72
Lucas Santos <lucasdafsantos@gmail.com>
2022-04-10 22:43:54 -0300

    chore: finalizando plugins

--- 3_PLUGINS/src/index.js
+++ 3_PLUGINS/src/index.js
@@ -2,3 +2,7 @@ import Heading from './components/heading'
 
 const h = new Heading()
 h.create('Testando')
+
+console.log(VERSION)
+console.log(PORT)
+console.log(process.env.API_KEY)
--- 3_PLUGINS/webpack.config.js
+++ 3_PLUGINS/webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path')
 const terserPlugin = require('terser-webpack-plugin')
 const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const { DefinePlugin } = require('webpack')
+const dotenv = require('dotenv-webpack')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].[contenthash].js' // Concertar problema de cache (Precisa do plugin htmlWebpackPlugin)
     },
     mode: 'development',
     optimization: { /* Minificar Js Gerado (Já faz isso em prod nativamente) */
@@ -24,8 +28,16 @@ module.exports = {
         ]
     },
     plugins: [
-        new miniCssExtractPlugin({ /* Adiciona o plugin JS */
-            filename: 'styles.css' 
-        })
+        new CleanWebpackPlugin(), /* Limpa arquivos desnecessários na pasta de build */
+        new miniCssExtractPlugin({ /* Minifica o CSS */
+            filename: '[name].[contenthash].css'  // Concertar problema de cache (Precisa do plugin htmlWebpackPlugin)
+        }),
+        new DefinePlugin({ /* Adiciona constantes globais */
+            VERSION: JSON.stringify('1.0.1'),
+            PORT: JSON.stringify(8080)
+        }),
+        new dotenv(),
+        new htmlWebpackPlugin() // Buildar o index.html,
+
     ]
 }
